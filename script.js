38120f34c9656021d49bc73a9d11a88750c80699
zeckyon <129232416+zeckyon@users.noreply.github.com>
2023-04-05 21:59:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Meu comentario
+console.log("Olá Mundo");
+
+//prompt("Qual a sua idade ?")
+
+const diaDeNascimento = "15/08/1997";
+let texto = "Minha data de nascimento é: ";
+console.log(diaDeNascimento);
+console.log(texto);
+
+//Concatenando variaveis
+console.log(texto + diaDeNascimento);
+
+// Exemplos
+const primeiroNome = "Rodrigo";
+const segundoNome = "Zeck";
+let minhaIdade = "25";
+let estaEstudando = true;
+
+//imprimir
+console.log("Meu nome é "+ primeiroNome + " " + segundoNome + " " + "Tenho " + minhaIdade + ", e estou estudando " + estaEstudando);
